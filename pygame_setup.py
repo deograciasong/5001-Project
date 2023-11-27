2b89fdc8f6cda47413993bd44058b4970ba3b1d8
YillonMask <yixinrui24@gmail.com>
2023-11-27 12:17:43 -0800

    pygame_setup

---
+++ pygame_setup.py
@@ -0,0 +1,18 @@
+# black jack in python wth pygame!
+import copy
+import random
+import pygame
+
+pygame.init()
+# game variables
+cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+one_deck = 4 * cards
+decks = 4
+WIDTH = 600
+HEIGHT = 900
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption('Pygame Blackjack!')
+fps = 60
+timer = pygame.time.Clock()
+font = pygame.font.Font('freesansbold.ttf', 44)
+smaller_font = pygame.font.Font('freesansbold.ttf', 36)
