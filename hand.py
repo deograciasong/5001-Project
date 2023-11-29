93ff244f5ff80826eea0cb402f2f17692b30ec3e
deograciasong <77689676+deograciasong@users.noreply.github.com>
2023-11-29 15:39:24 -0800

    hand update

--- hand.py
+++ hand.py
@@ -3,14 +3,23 @@
     creating the hand class
 """
 
+
 class Hand:
-    ''' class: Hand
+    """ class: Hand
         Attributes: cards, value
         Methods: add_card (appends a new card to cards)
                  calc_value (calculates the value of the hand)
-    '''
+    """
 
+    def __init__(self, cards=None, value=0):
+        """
+            Constructor -- creates a new instance of a hand
+            Parameters: self -- the current object
+                        cards -- a list of cards in the hand
+                        value -- value of the hand
+        """
+        if cards is None:
+            cards = []
 
-    def __init__(self, cards, value):
         self.cards = cards
         self.value = value
