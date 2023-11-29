c1dab75e8294aa50cc25d44e87738e6930456a01
deograciasong <77689676+deograciasong@users.noreply.github.com>
2023-11-29 15:57:37 -0800

    framework for deck

---
+++ deck.py
@@ -0,0 +1,18 @@
+"""
+    deck.py
+    creating the hand class
+"""
+decks = 8
+dict_remaining_cards = {"2": 4 * decks, "3": 4 * decks, "4": 4 * decks,
+                        "5": 4 * decks, "6": 4 * decks, "7": 4 * decks,
+                        "8": 4 * decks, "9": 4 * decks, "10": 4 * decks,
+                        "J": 4 * decks, "Q": 4 * decks, "K": 4 * decks,
+                        "A": 4 * decks}
+
+class Deck:
+    """ class: Deck
+        Attributes:
+        Methods: draw_card (removes card from deck)
+                 shuffle
+                 remaining_count
+    """
