923331932763d0a6d05bea190f5b3d0c254bdc4d
deograciasong <77689676+deograciasong@users.noreply.github.com>
2023-11-29 22:49:13 -0800

    updates

--- pygame_mainframe.py
+++ pygame_mainframe.py
@@ -5,18 +5,10 @@ from hand import Hand
 
 pygame.init()
 
+
 def main():
     run = True
     decks = 8
-    dict_card_value = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6,
-                       "7": 7, "8": 8, "9": 9, "10": 10, "J": 10,
-                       "Q": 10, "K": 10, "A": 11}
-
-    dict_remaining_cards = {"2": 4 * decks, "3": 4 * decks, "4": 4 * decks,
-                            "5": 4 * decks, "6": 4 * decks, "7": 4 * decks,
-                            "8": 4 * decks, "9": 4 * decks, "10": 4 * decks,
-                            "J": 4 * decks, "Q": 4 * decks, "K": 4 * decks,
-                            "A": 4 * decks}
 
     shoe = 52 * decks
     cut_off = shoe * .20
@@ -39,7 +31,6 @@ def main():
     bank = int(input("How much money are you willing to lose?"
                      "(gamble responsibly!)"))
 
-
     while (shoe > cut_off) and run:
         # generate user input for bet
         bet = int(input("what is your bet size for the round?"))
@@ -56,23 +47,31 @@ def main():
         # display both hands
 
         # create a loop for player action
-            # generate user input to determine action
-            # display new player hand
-
+        # generate user input to determine action
+        # display new player hand
+        if player.get_value > 21:
+            losses += 1
+            bank -= bet
+            rounds += 1
 
         # create loop for dealer action
-            # hit until can no longer hit or bust
-            # display new dealer hand
-
-        # determine who won
-        # calculate the remaining balance of the player
-        if player.get_value > dealer.get_value:
+        # hit until can no longer hit or bust
+        # display new dealer hand
+        if dealer.get_value > 21:
             wins += 1
             bank += bet
             rounds += 1
-        elif player.get_value < dealer.get_value:
+            continue
+
+        # determine who won
+        # calculate the remaining balance of the player\
+        if player.get_value < dealer.get_value:
             losses += 1
             bank -= bet
+            rounds += 1
+        elif player.get_value > dealer.get_value:
+            wins += 1
+            bank += bet
             rounds += 1
         else:
             pushes += 1
@@ -80,12 +79,12 @@ def main():
 
         # update statistics and visualize
 
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
