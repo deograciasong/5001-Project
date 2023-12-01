43a8ab86431bbfe83caae2e6a2a173553af1fc29
Byunghyun Ko <benedict.ko2018@gmail.com>
2023-11-30 16:03:25 -0800

    Update deck.py

--- deck.py
+++ deck.py
@@ -38,11 +38,7 @@ class Deck:
 
     def shuffle(self):
         # creating full 52*8 deck again
-        self.remaining_cards = {"2": 4 * decks, "3": 4 * decks, "4": 4 * decks,
-                        "5": 4 * decks, "6": 4 * decks, "7": 4 * decks,
-                        "8": 4 * decks, "9": 4 * decks, "10": 4 * decks,
-                        "J": 4 * decks, "Q": 4 * decks, "K": 4 * decks,
-                        "A": 4 * decks}
+        self.remaining_cards = dict_remaining_cards
 
     def remaining_count(self):
         return self.drawn_count
